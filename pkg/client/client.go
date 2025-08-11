8e571c89932de47e3ce06e2af78917c0c7a916df
Farruh4eg <farruh4eg@gmail.com>
2025-08-11 19:39:33 +0300

    feat: add enemy hp progress bar

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,6 +4,7 @@ package client
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	pb "clicker/gen/proto"
 
@@ -11,22 +12,26 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 type ClickerApp struct {
-	stream    pb.GameService_PlayGameClient
-	player    *pb.Player
-	enemyName binding.String
-	enemyHp   binding.String
+	stream         pb.GameService_PlayGameClient
+	player         *pb.Player
+	enemyName      binding.String
+	enemyCurrentHp binding.Float
+	enemyMaxHp     binding.Float
+	enemyHpBar     widget.ProgressBar
 }
 
 func NewClickerApp(stream pb.GameService_PlayGameClient, player *pb.Player) *ClickerApp {
 	return &ClickerApp{
-		stream:    stream,
-		player:    player,
-		enemyName: binding.NewString(),
-		enemyHp:   binding.NewString(),
+		stream:         stream,
+		player:         player,
+		enemyName:      binding.NewString(),
+		enemyCurrentHp: binding.NewFloat(),
+		enemyMaxHp:     binding.NewFloat(),
 	}
 }
 
@@ -49,12 +54,13 @@ func (a *ClickerApp) Run() {
 			log.Printf("Got a message from server: %v", in.GetInitialState())
 			if update := in.GetGameStateUpdate(); update != nil {
 				log.Printf("GAME STATE UPDATE: Boss HP = %.2f, Last Attacker ID = %s", update.GetEnemyCurrentHp(), update.LastHit.GetAttackerId())
-				a.enemyHp.Set(fmt.Sprintf("%.2f", update.GetEnemyCurrentHp()))
+				a.enemyCurrentHp.Set(update.GetEnemyCurrentHp())
 			}
 			if initState := in.GetInitialState(); initState != nil {
 				log.Printf("INITIAL STATE: Boss Name = %s, HP = %.2f", initState.GetEnemy().Name, initState.Enemy.GetMaxHp())
 				a.enemyName.Set(initState.GetEnemy().GetName())
-				a.enemyHp.Set(fmt.Sprintf("%.2f", initState.GetEnemy().GetCurrentHp()))
+				a.enemyCurrentHp.Set(initState.GetEnemy().GetCurrentHp())
+				a.enemyMaxHp.Set(initState.GetEnemy().GetMaxHp())
 			}
 		}
 	}()
@@ -78,12 +84,61 @@ func (a *ClickerApp) Run() {
 
 	enemyNameLabel := widget.NewLabelWithData(a.enemyName)
 
-	enemyHpLabel := widget.NewLabelWithData(a.enemyHp)
+	// TODO: implement enemy hp as a slider
+	enemyHpBar := widget.NewProgressBar()
 
-	enemyLayout := container.NewGridWithRows(3, enemyNameLabel, enemyHpLabel, attackButton)
+	enemyHpLabel := widget.NewLabel("")
+	enemyHpLabel.Alignment = fyne.TextAlignCenter
+
+	hpListener := binding.NewDataListener(func() {
+		current, errCurrent := a.enemyCurrentHp.Get()
+		max, errMax := a.enemyMaxHp.Get()
+
+		if errCurrent != nil || errMax != nil {
+			log.Printf("Error getting current/max hp values: \n%v\n%v", errCurrent, errMax)
+			return
+		}
+
+		hpText := fmt.Sprintf("%.0f / %.0f", current, max)
+		enemyHpLabel.SetText(hpText)
+
+		if max > 0 {
+			normalizedHp := current / max
+			enemyHpBar.SetValue(normalizedHp)
+		}
+	})
+
+	a.enemyCurrentHp.AddListener(hpListener)
+	a.enemyMaxHp.AddListener(hpListener)
+
+	enemyLayout := container.NewVBox(
+		container.NewCenter(enemyNameLabel),
+		// TODO: insert image here
+
+		enemyHpBar,
+
+		layout.NewSpacer(),
+		attackButton,
+	)
 
 	w.SetContent(enemyLayout)
 
+	hpListener.DataChanged()
+
 	w.ShowAndRun()
 	log.Println("Application shutting down")
+}
+
+func BindingStrToFloat64(s binding.String) float64 {
+	data, err := s.Get()
+	if err != nil {
+		log.Printf("Could not get binding data: %v", err)
+	}
+
+	dataAsFloat, err := strconv.ParseFloat(data, 64)
+	if err != nil {
+		log.Printf("Could not parse data as float: %v", err)
+	}
+
+	return dataAsFloat
 }
